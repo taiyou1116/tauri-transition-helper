2b02081f220835bbdcccb5e4d355ed047d1395e7
taiyou <hiromacha1116@icloud.com>
2023-09-15 14:42:12 +0900

    clipboard -> monitor_clipboard

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use dotenv::dotenv;
-mod clipboard;
 mod config;
+mod monitor_clipboard;
 mod transition;
 
 #[tokio::main]
@@ -11,7 +11,7 @@ async fn main() {
     dotenv().ok();
     let config = config::Config::new().expect("Failed to load config");
 
-    clipboard::run(&config.api_key).await;
+    monitor_clipboard::run(&config.api_key).await;
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
