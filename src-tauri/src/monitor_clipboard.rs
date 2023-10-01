b68c23cd4d58274fc2b6ab357200319b8fb696ff
taiyou <hiromacha1116@icloud.com>
2023-10-02 00:26:17 +0900

    spawnエラー解消

--- src-tauri/src/monitor_clipboard.rs
+++ src-tauri/src/monitor_clipboard.rs
@@ -23,32 +23,36 @@ pub async fn run(api_key: &str, client: &Arc<reqwest::Client>) {
     };
 
     let mut last_clipboard_content = String::new();
+    let mut content = None;
 
     // let client = reqwest::Client::new();
     // let client = Arc::new(client); // Arcでラップする
 
     loop {
         // クリップボードからテキストを取得
-        match ctx.get_contents() {
-            Ok(contents) => {
-                if last_clipboard_content != contents {
-                    // クリップボードの内容が前回と異なる場合
-                    println!("New clipboard content: {}", contents);
-                    last_clipboard_content = contents;
+        content = match ctx.get_contents() {
+            Ok(contents) => Some(contents),
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                None // エラーが発生した場合はNoneを設定
+            }
+        };
 
-                    // ここで翻訳とデスクトップ通知を行う
-                    match transition::run(&api_key, &last_clipboard_content, &client).await {
-                        Ok(_) => {
-                            // 通知
-                        }
-                        Err(e) => {
-                            eprintln!("Error: {}", e.to_string());
-                        }
+        // `content` が Some(value) であり、`last_clipboard_content` と異なる場合
+        if content.as_ref() != Some(&last_clipboard_content) {
+            if let Some(c) = content {
+                println!("New clipboard content: {}", c);
+                last_clipboard_content = c;
+
+                // ここで翻訳とデスクトップ通知を行う
+                match transition::run(&api_key, &last_clipboard_content, &client).await {
+                    Ok(_) => {
+                        // 通知
+                    }
+                    Err(e) => {
+                        eprintln!("Error: {}", e.to_string());
                     }
                 }
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e.to_string());
             }
         }
 
